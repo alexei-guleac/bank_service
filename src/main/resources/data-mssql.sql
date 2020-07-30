8c2b65350dc4c3d8e198319cf80a0bdd3a1c9e33
al.guleac <al.guleac@gmail.com>
2020-07-30 13:33:28 +0300

    optional database initialization at application startup via data-mssql.sql

---
+++ src/main/resources/data-mssql.sql
@@ -0,0 +1,3 @@
+INSERT INTO bank_account (id, user_id, account_number, currency, last_withdrawal) VALUES (1, 12345, '454322443dfdf', 0, '2020-07-06 18:16:29.668000');
+INSERT INTO bank_account (id, user_id, account_number, currency, last_withdrawal) VALUES (2, 123, '4543reerfgfg2', 1, '2020-07-15 13:15:29.668000');
+INSERT INTO bank_account (id, user_id, account_number, currency, last_withdrawal) VALUES (3, 12345, '1234454556uii', 2, '2020-07-25 17:16:29.668000');
